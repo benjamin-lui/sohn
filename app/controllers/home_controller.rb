9b71f25fc9d6c256ffb30bde59290e362bd5efd1
Benjamin Lui <benlui@berkeley.edu>
2013-03-09 15:10:38 -0800

    init

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,16 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def research
+  end
+
+  def people
+  end
+
+  def publications
+  end
+
+  def positions
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sohn::Application.config.secret_token = '908846fd4ded45da872bfd3ea4acba2277642ec583c7b960cf62b91e7bd78cba010f559af4a4b9aa5e10e3d112216ab384d8ddb7cf2c393fcf3647c986d19e8f'
